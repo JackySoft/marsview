21b28af4f183c2ea4762651cd8f9648a32dbbd85
jianbing.chen <jianbing.chen@huolala.cn>
2024-08-05 19:50:42 +0800

    build: 打包

--- dist/admin/assets/index-BJNQkd_v.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Bi1Gsx-r.js";const r=window.antd.Image,n=window.React.useState,i=window.React.useImperativeHandle,w=window.React.forwardRef,d=({config:e},s)=>{const[a,t]=n(!0);return i(s,()=>({show(){t(!0)},hide(){t(!1)}})),a&&o.jsx(r,{style:e.style,...e.props})},c=w(d);export{c as Image};
--- dist/editor/assets/pageMember-DZklxqqw.js
+++
@@ -1,1 +0,0 @@
-import{T as a}from"./index-CSGxKpEX.js";const s={getMemberList(e){return a.post("/page/role/list",e,{showLoading:!1})},addPageMember(e){return a.post("/page/role/add",e,{showLoading:!1})},deletePageMember(e){return a.post("/page/role/delete",e)},getPageList(){return a.post("/page/user/pages",{},{showLoading:!1})}};export{s as a};
