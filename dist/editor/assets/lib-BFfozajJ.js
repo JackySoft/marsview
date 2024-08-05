38448a3966269adf821c6a049db70dc0f9f63d57
河畔一角 <sunnyboysoft@163.com>
2024-08-05 17:33:14 +0800

    build: 打包

---
+++ dist/editor/assets/lib-BFfozajJ.js
@@ -0,0 +1,1 @@
+import{R as s}from"./index-BBkS68sv.js";const e=t=>s.get("/lib/list",t,{showLoading:!1}),a=()=>s.post("/lib/install/list",{},{showLoading:!1}),l=t=>s.get(`/lib/detail/${t}`),o=t=>s.post("/lib/update",t),n=t=>s.post("/lib/publish",t);export{l as a,a as b,e as g,n as p,o as u};
