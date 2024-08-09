bd40a887f8fc38d496ad06dabb747eec6c49bba1
jianbing.chen <jianbing.chen@huolala.cn>
2024-08-09 17:04:56 +0800

    build: 资源打包

--- dist/admin/assets/index-nsDw7TTR.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-BMoS0DUA.js";const o=window.React.useState,n=window.React.useImperativeHandle,i=window.React.forwardRef,m=({config:e},s)=>{const[r,t]=o(!0);return n(s,()=>({show(){t(!0)},hide(){t(!1)}})),r&&a.jsx("iframe",{style:e.style,...e.props})},w=i(m);export{w as IFrame};
