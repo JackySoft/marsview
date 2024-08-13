6cee4a179ca2a95f6b121b4723efa167988037ac
jianbing.chen <jianbing.chen@huolala.cn>
2024-08-13 15:35:20 +0800

    fix: 修改本地端口

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: '../../dist/admin',
   },
   server: {
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 8090,
     headers: {
       'Access-Control-Allow-Origin': '*',
