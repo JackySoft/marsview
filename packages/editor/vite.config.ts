474ca890c9c7b3795d1c9abcf0eab3133f92465b
JackySoft <sunnyboysoft@163.com>
2024-08-15 15:00:00 +0800

    fix: 修复热更新问题

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { VitePluginExternals } from 'vite-plugin-externals-new';
@@ -29,32 +29,33 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
-    VitePluginExternals({
-      react: {
-        src: 'https://cdn.staticfile.net/react/18.2.0/umd/react.production.min.js',
-        varName: 'React',
-      },
-      'react-dom': {
-        src: 'https://cdn.staticfile.net/react-dom/18.2.0/umd/react-dom.production.min.js',
-        varName: 'ReactDOM',
-      },
-      dayjs: {
-        src: 'https://cdn.staticfile.net/dayjs/1.11.10/dayjs.min.js',
-        varName: 'dayjs',
-      },
-      antd: {
-        src: 'https://cdn.staticfile.net/antd/5.13.2/antd.min.js',
-        varName: 'antd',
-      },
-      ahooks: {
-        src: 'https://marsview.cdn.bcebos.com/static/ahooks.js',
-        varName: 'ahooks',
-      },
-      axios: {
-        src: 'https://cdn.staticfile.net/axios/1.6.5/axios.min.js',
-        varName: 'axios',
-      },
-    }),
+    react() as PluginOption,
+    process.env.NODE_ENV === 'production' &&
+      VitePluginExternals({
+        react: {
+          src: 'https://cdn.staticfile.net/react/18.2.0/umd/react.production.min.js',
+          varName: 'React',
+        },
+        'react-dom': {
+          src: 'https://cdn.staticfile.net/react-dom/18.2.0/umd/react-dom.production.min.js',
+          varName: 'ReactDOM',
+        },
+        dayjs: {
+          src: 'https://cdn.staticfile.net/dayjs/1.11.10/dayjs.min.js',
+          varName: 'dayjs',
+        },
+        antd: {
+          src: 'https://cdn.staticfile.net/antd/5.13.2/antd.min.js',
+          varName: 'antd',
+        },
+        ahooks: {
+          src: 'https://marsview.cdn.bcebos.com/static/ahooks.js',
+          varName: 'ahooks',
+        },
+        axios: {
+          src: 'https://cdn.staticfile.net/axios/1.6.5/axios.min.js',
+          varName: 'axios',
+        },
+      }),
   ],
 });
