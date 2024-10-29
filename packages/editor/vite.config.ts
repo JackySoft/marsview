9b8dbcf1a599f51f14c8a91dc7fe554dda21c741
yangshare <717449318@qq.com>
2024-10-29 22:21:09 +0800

    增加配置示例

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://mars-api.marsview.cc',
+        target: 'http://localhost:9001/api',
         changeOrigin: true,
       },
     },
