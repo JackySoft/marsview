d533ddf7acd91fbc2a1cf87bf69f687e7a514223
河畔一角 <sunnyboysoft@163.com>
2024-11-07 21:29:28 +0800

    feat: 迁移后端仓库

--- backend/koa/main.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { SERVER_HOST, SERVER_PORT } = require('./config');
-
-app.listen(SERVER_PORT, () => {
-  console.log(`Marsview服务已启动 ${SERVER_HOST}:${SERVER_PORT}`);
-});
