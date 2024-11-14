e4b9fd83c37baa16efd3739a43960a4586c531fb
waiterxiaoyy <738294518@qq.com>
2024-11-14 22:35:52 +0800

    20241114feedback

--- packages/editor/src/pages/feedback/index.tsx
+++ packages/editor/src/pages/feedback/index.tsx
@@ -1,336 +1,191 @@
-import { Col, Pagination, Row } from 'antd';
-import { Input } from 'antd';
-const { Search } = Input;
-import styles from './index.module.less';
-import type { GetProps } from 'antd';
-import { useEffect, useState } from 'react';
-import { FeedbackItem, ItemCardProps } from '../types';
-import ItemCard from './components/ItemCard';
+import React from 'react';
+import { Layout, Typography, Tag, Button, Space, List, Tabs, Input, Avatar, Pagination } from 'antd';
+import { EyeOutlined, ShareAltOutlined, CalendarOutlined, SearchOutlined, LikeOutlined, HeartOutlined } from '@ant-design/icons';
+import style from './index.module.less';
 import { useNavigate } from 'react-router-dom';
-type SearchProps = GetProps<typeof Input.Search>;
 
-const FeedbackIndex = () => {
+const { Content } = Layout;
+const { TabPane } = Tabs;
+const { Title, Paragraph, Text } = Typography;
 
-  const navigte = useNavigate();
 
-  const navList = [
-    {
-      label: '全部',
-      id: 'all',
-    },
-    {
-      label: 'Bug',
-      id: 'bug',
-    },
-    {
-      label: '建议',
-      id: 'advise',
-    },
-    {
-      label: '其他',
-      id: 'other',
-    },
-  ];
+interface QAItem {
+  id: string;
+  avatar: string;
+  title: string;
+  tags: string[];
+  username: string;
+  likes: number;
+  publishTime: string;
+}
 
-  const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+const mockData: QAItem[] = [
+  {
+    id: '1',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    title: '新增反馈功能',
+    tags: ['Bug'],
+    username: 'Garrett',
+    likes: 15,
+    publishTime: '2024-10-08 08:30',
+  },
+  {
+    id: '2',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    title: '如何提高编程效率？',
+    tags: ['建议'],
+    username: '白码',
+    likes: 32,
+    publishTime: '2024-09-29 12:01',
+  },
+  {
+    id: '3',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    title: '推荐一些好用的设计工具',
+    tags: ['前端'],
+    username: 'DesignMaster',
+    likes: 28,
+    publishTime: '2024-09-15 15:45',
+  },
+  {
+    id: '4',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    title: '推荐一些好用的设计工具',
+    tags: ['设计', '工具'],
+    username: 'DesignMaster',
+    likes: 28,
+    publishTime: '2024-09-15 15:45',
+  },
+  {
+    id: '5',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    title: '推荐一些好用的设计工具',
+    tags: ['设计', '工具'],
+    username: 'DesignMaster',
+    likes: 28,
+    publishTime: '2024-09-15 15:45',
+  },
+];
+const DailyFresh2024: React.FC = () => {
 
-  const [selectedId, setSelectedId] = useState('all');
-
-  const handleItemClick = (id: string) => {
-    setSelectedId(id);
-  };
-
-  const feedbackList: FeedbackItem[] = [
-    {
-      id: 1,
-      title: '页面响应',
-      content: '问题内容',
-      isSolve: true,
-      isTop: true,
-      like: 17,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'bug',
-    },
-    {
-      id: 2,
-      title: '希望新增反馈功能',
-      content: '问题内容',
-      isSolve: false,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'advise',
-    },
-    {
-      id: 3,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 4,
-      title: '页面响应',
-      content: '问题内容',
-      isSolve: true,
-      isTop: true,
-      like: 20,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'bug',
-    },
-    {
-      id: 5,
-      title: '希望新增反馈功能',
-      content: '问题内容',
-      isSolve: false,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'advise',
-    },
-    {
-      id: 6,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 7,
-      title: '页面响应',
-      content: '问题内容',
-      isSolve: true,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'bug',
-    },
-    {
-      id: 8,
-      title: '希望新增反馈功能',
-      content: '问题内容',
-      isSolve: false,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'advise',
-    },
-    {
-      id: 9,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 10,
-      title: '页面响应',
-      content: '问题内容',
-      isSolve: true,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'bug',
-    },
-    {
-      id: 11,
-      title: '希望新增反馈功能',
-      content: '问题内容',
-      isSolve: false,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'advise',
-    },
-    {
-      id: 12,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 13,
-      title: '页面响应',
-      content: '问题内容',
-      isSolve: true,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'bug',
-    },
-    {
-      id: 14,
-      title: '希望新增反馈功能',
-      content: '问题内容',
-      isSolve: false,
-      isTop: true,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'advise',
-    },
-    {
-      id: 15,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 16,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 17,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 0,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-    {
-      id: 18,
-      title: '问题标题',
-      content: '问题内容',
-      isSolve: true,
-      isTop: false,
-      like: 4,
-      userAvatar: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      time: '2021-09-01',
-      userName: '用户名',
-      type: 'other',
-    },
-  ];
-
-  const [filteredList, setFilteredList] = useState<FeedbackItem[]>([]);
-
-  useEffect(() => {
-    if (feedbackList && feedbackList.length > 0) {
-      // 将置顶的isTop放在前面
-      const sortedList = feedbackList.sort((a, b) => {
-        if (a.isTop && !b.isTop) return -1;
-        if (!a.isTop && b.isTop) return 1;
-        return 0;
-      });
-
-      // 根据时间排序
-      sortedList.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
-
-      // 根据selectedId筛选列表中type符合的项
-      const filteredList = sortedList.filter((item) => item.type === selectedId);
-
-      setFilteredList(filteredList);
-    }
-  }, [feedbackList, selectedId]);
-
-  useEffect(() => {
-    setFilteredList(feedbackList);
-  }, [feedbackList]);
-
-  const handleIssueClick = (item: FeedbackItem) => {
-    navigte(`/feedback/${item.id}/detail`);
+  const navigate = useNavigate();
+  const handleCardClick = (id: string) => {
+    console.log('Card Clicked:', id);
+    navigate(`/feedback/${id}/detail`);
   }
 
+  const tabs = [
+    {
+      key: '1',
+      label: '全部',
+    },
+    {
+      key: '2',
+      label: '建议'
+    },
+    {
+      key: '3',
+      label: 'Bug'
+    },
+    {
+      key: '4',
+      label: '前端'
+    },
+    {
+      key: '5',
+      label: '设计'
+    },
+    {
+      key: '6',
+      label: '其他'
+    },
+    {
+      key: '7',
+      label: 'Github'
+    }
+  ]
   return (
-    <Row>
-      <Col span={6}></Col>
-      <Col span={12}>
-        <div className={styles.feedback}>
-          <div className={styles.title}>问答专栏</div>
-          <div className={styles.navBar}>
-            <div className={styles.navBody}>
-              <div className={`${styles.navItem} ${selectedId === 'me' ? styles.selected : ''}`} onClick={() => handleItemClick('me')}>
-                只看自己
+    <div className={style.layout}>
+      <div className={style.content}>
+        <div className={style.header}>
+          <div className={style.headerContent}>
+            <div className={style.headerLeft}>
+              <Title level={2} className={style.title}>问答专栏</Title>
+              <Space className={style.tags}>
+                <Tag>用户声音</Tag>
+                <Tag>创新分享</Tag>
+                <Tag>反馈建议</Tag>
+              </Space>
+              <Paragraph className={style.description}>
+              无论是解惑、反馈，还是分享创新的火花，都在此得到珍视～
+              </Paragraph>
+            </div>
+            <div className={style.headerRight}>
+              <div className={style.statCard}>
+                <Text className={style.statNumber}>568</Text>
+                <Text className={style.statLabel}>建议</Text>
+              </div>
+              <div className={style.statCard}>
+                <Text className={style.statNumber}>1万</Text>
+                <Text className={style.statLabel}>解决</Text>
               </div>
             </div>
-            <div className={styles.navBody}>
-              {navList.map((item) => (
-                <div
-                  key={item.id}
-                  className={`${styles.navItem} ${selectedId === item.id ? styles.selected : ''}`}
-                  onClick={() => handleItemClick(item.id)}
-                >
-                  {item.label}
-                </div>
-              ))}
-            </div>
-            <div className={styles.navBody}>
-              <div className={`${styles.navItem} ${selectedId === 'github' ? styles.selected : ''}`} onClick={() => handleItemClick('github')}> Github</div>
-            </div>
-            <div className={styles.navBody}>
-              <Search size="middle" placeholder="搜索问题" onSearch={onSearch} enterButton />
-            </div>
           </div>
-          <div className={styles.content}>
-            {filteredList.map((item) => (
-              <ItemCard key={item.id} info={item} click={() => handleIssueClick(item)} />
-            ))}
-          </div>
-          <div className={styles.footer}>
-            <Pagination defaultCurrent={1} total={50} />
-          </div>
+          <Space className={style.headerActions}>
+            <Button type="primary" className={style.followButton}>+ 发布</Button>
+            <Button icon={<ShareAltOutlined />} className={style.shareButton}>分享</Button>
+          </Space>
         </div>
-      </Col>
-      <Col span={6}></Col>
-    </Row>
+        <div className={style.tabsContainer}>
+          <Tabs defaultActiveKey="1" items={tabs} className={style.tabs}>
+
+          </Tabs>
+          <Input
+            placeholder="搜索问题"
+            prefix={<SearchOutlined />}
+            className={style.searchInput}
+          />
+        </div>
+        <div className={style.listContent}>
+          <Title level={3} className={style.listTitle}>社区</Title>
+          <List
+            itemLayout="horizontal"
+            dataSource={mockData}
+            renderItem={(item) => (
+              <List.Item className={style.qaCard}>
+                <div className={style.cardContent} onClick={()=>handleCardClick(item.id)}>
+                  <Avatar src={item.avatar} size={48} className={style.avatar} />
+                  <div className={style.middleContent}>
+                    <div className={style.itemTitle}>{item.title}</div>
+                    <Space size={[0, 8]} wrap>
+                      {item.tags.map((tag) => (
+                        <Tag key={tag} className={style.tag}>
+                          {tag}
+                        </Tag>
+                      ))}
+                    </Space>
+                  </div>
+                  <div className={style.rightContent}>
+                    <div className={style.userInfo}>
+                      <span className={style.username}>{item.username}</span>
+                      <div>
+                        <HeartOutlined />
+                        <span> {item.likes}</span>
+                      </div>
+                    </div>
+                    <div className={style.publishTime}>{item.publishTime}</div>
+                  </div>
+                </div>
+              </List.Item>
+            )}
+          />
+        </div>
+        <div className={style.pageControl}>
+          <Pagination defaultCurrent={1} total={50} />
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default FeedbackIndex;
+export default DailyFresh2024;
