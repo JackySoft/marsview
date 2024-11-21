d3ab19099420fdafd6e26bc32a5ff80d9563b688
FLY200117 <1812809429@qq.com>
2024-11-21 18:07:52 +0800

    fix(feat/rate)：提交评分组件

--- packages/editor/src/packages/components/IConSetting.tsx
+++ packages/editor/src/packages/components/IConSetting.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Form, Select } from 'antd';
 import * as icons from '@ant-design/icons';
-
+export const iconsList: { [key: string]: any } = icons;
 /**
  * 公共图标设置
  */
-export default function IConSetting({ name = 'icon' }: { name?: string | string[] }) {
+export default function IConSetting({ name = 'icon', initalValue }: { name?: string | string[], initalValue?: string }) {
   // 获取所有的antd图标，动态渲染到下拉框中
-  const iconsList: { [key: string]: any } = icons;
   return (
-    <Form.Item label="按钮图标" name={name}>
+    <Form.Item label="按钮图标" name={name} initialValue={initalValue}>
       <Select placeholder="请选择菜单图表" showSearch allowClear>
         {Object.keys(icons)
           .filter((item) => !['default', 'createFromIconfontCN', 'getTwoToneColor', 'setTwoToneColor', 'IconProvider'].includes(item))
